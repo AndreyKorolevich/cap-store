b3bd6ea2d56991b09923a4d98cf706befaec6219
AndreyKorolevich <korolevich1994@mail.ru>
2021-08-20 12:22:11 -0700

    update filter endpoint

--- server/server.js
+++ server/server.js
@@ -8,9 +8,7 @@ const koaBody = require('koa-body');
 const brand = JSON.parse(fs.readFileSync('./data/brand.json'));
 const gender = JSON.parse(fs.readFileSync('./data/gender.json'));
 const items = JSON.parse(fs.readFileSync('./data/products.json'));
-const maxPrice = {
-    maxPrice: Math.max(...items.map(i => i.price))
-}
+const maxPrice = Math.max(...items.map(i => i.price))
 
 const itemBasicMapper = item => ({
     id: item.id,
@@ -46,32 +44,35 @@ router.get('/api/gender', async (ctx, next) => {
     return fortune(ctx, gender);
 });
 router.get('/api/maxprice', async (ctx, next) => {
-    return fortune(ctx, maxPrice);
+    const response = {
+        maxPrice: Math.max(...items.map(i => i.price))
+    }
+    return fortune(ctx, response);
 });
 
 router.get('/api/items', async (ctx, next) => {
-    const { query } = ctx.request;
+    const {query} = ctx.request;
 
     const brand = query.brand === undefined ? 0 : Number(query.brand);
     const page = query.page === undefined ? 0 : Number(query.page);
-    const countOnPage = query.countOnPage === undefined ? 0 : Number(query.countOnPage);
+    const countOnPage = query.countOnPage === undefined ? 10 : Number(query.countOnPage);
     const q = query.q === undefined ? '' : query.q.trim().toLowerCase();
     const gender = query.gender === undefined ? 0 : Number(query.brand);
-    const price = query.price === undefined ? maxPrice : Number(query.offset);
+    const price = query.price === undefined ? maxPrice : Number(query.price);
     const color = query.color === undefined ? 0 : Number(query.color);
 
     const startPage = page * countOnPage
     const endPage = startPage + countOnPage
 
     const filtered = items
-        // .filter(o => brand === 0 || o.brand === brand)
-        // .filter(o => gender === 0 || o.gender === gender)
-        // .filter(o => o.price <= price)
-        // .filter(o => o.title.toLowerCase().includes(q))
-        // .slice(startPage, endPage)
-        //.map(itemBasicMapper);
+        .filter(o => brand === 0 || o.brand === brand)
+        .filter(o => gender === 0 || o.gender === gender)
+        .filter(o => o.price <= price)
+        .filter(o => o.name.toLowerCase().includes(q))
 
-    return fortune(ctx, {filtered, itemsCount: items.length});
+    const paginated = filtered.slice(startPage, endPage)
+
+    return fortune(ctx, {paginated, itemsCount: filtered.length});
 });
 
 
