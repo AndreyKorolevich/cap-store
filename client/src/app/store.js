95616606cbfccc7bab3112ed4e69ea048206da62
AndreyKorolevich <korolevich1994@mail.ru>
2021-09-13 12:22:11 -0700

    refactor store

---
+++ client/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from '../features/cartManager'
+import storeReducer from '../features/storeManager'
+
+export default configureStore({
+  reducer: {
+    cart: cartReducer,
+    results: storeReducer,
+  },
+})
